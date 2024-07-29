c4d1662638b42da9ed95bde5da55795153cd1292
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-30 00:03:28 +0100

    Get user ip address

--- app.js
+++ app.js
@@ -20,8 +20,10 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+  });
 });
 
 module.exports = app;
