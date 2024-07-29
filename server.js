b8cc9522897962327af8b1a2c005902f34190c79
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-07-29 23:56:42 +0100

    Create server

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+
+// listen for requests :)
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
